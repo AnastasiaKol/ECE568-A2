594962a31eb99fbd5f36c95ccbbdef91c94ae8a0
AnastasiaKol <anastasia.kolesnikov@mail.utoronto.ca>
2019-03-02 19:18:59 -0500

    test

--- lab2/generateQRcode.c
+++ lab2/generateQRcode.c
@@ -24,8 +24,43 @@ main(int argc, char * argv[])
 
 	// Create an otpauth:// URI and display a QR code that's compatible
 	// with Google Authenticator
+	char * otp = "hotp";
+	int variableLength = strlen(otp)+strlen(accountName)+strlen(issuer)+strlen(secret_hex);
+	int invariableLength = strlen("otpauth://")+strlen("?issuer=")+strlen("&secret=")+strlen("counter=1")+4;
+	// char * URI = malloc(variableLength+invariableLength);
+	// sprintf(URI, "otpauth://%s/%s?issuer=%s&secret=%s&%s",
+	// 	otp,
+	// 	accountName,
+	// 	issuer,
+	// 	secret_hex,
+	// 	otp=="hotp" ? "counter=1" : "period=30"
+	// );
+	// char URI[100] = "";
+	// strcat(URI,"otpauth://");
+	// strcpy(strcpy(strcpy(strcpy(strcpy(strcpy(strcpy(strcpy(strcpy(strcpy(strcpy(
+	// 	URI,"otpauth://"),
+	// 	otp),
+	// 	"/"),
+	// 	accountName),
+	// 	"?issuer="),
+	// 	issuer),
+	// 	"&secret="),
+	// 	secret_hex),
+	// 	"&"),
+	// 	otp=="hotp" ? "counter=1" : "period=30"),
+	// 	"\0");
+	// printf("URI: %s", URI);
 
-	displayQRcode("otpauth://testing");
+	char src[50], dest[50];
+
+  strcpy(src,  "This is source");
+  strcpy(dest, "This is destination");
+
+  strcat(dest, src);
+
+  printf("Final destination string : |%s|", dest);
+
+	// displayQRcode("otpauth://testing");
 
 	return (0);
 }
